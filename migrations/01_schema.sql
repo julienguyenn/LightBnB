d4a4bacd43885237a2365243c7e119263837dc97
Julie Nguyen <julie-nguyen@live.com>
2020-01-08 19:17:22 +0000

    downloaded file

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,4 +1,9 @@
 
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS properties CASCADE;
+DROP TABLE IF EXISTS reservations CASCADE;
+DROP TABLE IF EXISTS property_reviews CASCADE;
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
@@ -14,13 +19,13 @@ CREATE TABLE properties (
   thumbnail_photo_url VARCHAR(255) NOT NULL,
   cover_photo_url VARCHAR(255) NOT NULL,
   cost_per_night INTEGER NOT NULL DEFAULT 0,
-  street VARCHAR(255) NOT NULL,
   parking_spaces INTEGER NOT NULL DEFAULT 0,
   number_of_bathrooms INTEGER NOT NULL DEFAULT 0,
   number_of_bedrooms INTEGER NOT NULL DEFAULT 0,
   country VARCHAR(255) NOT NULL,
   city VARCHAR(255) NOT NULL,
   province VARCHAR(255) NOT NULL,
+  street VARCHAR(255) NOT NULL,
   post_code VARCHAR(255) NOT NULL,
   active BOOLEAN NOT NULL DEFAULT TRUE
 );
@@ -49,7 +54,6 @@ CREATE TABLE guest_reviews (
   rating SMALLINT NOT NULL DEFAULT 0,
   message TEXT
 );
-
 
 
 CREATE TABLE property_reviews (
