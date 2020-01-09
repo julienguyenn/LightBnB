79d28854efb80c920be939e9e6391c252173de8a
Julie Nguyen <julie-nguyen@live.com>
2020-01-09 20:04:31 +0000

    add filter options for searching up properties

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -87,10 +87,65 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool.query(`
-  SELECT * FROM properties
-  LIMIT $1
-  `, [limit])
+  const queryParams = [];
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city ILIKE $${queryParams.length} `;
+  }
+
+  if (options.owner_id) {
+    queryParams.push(options.owner_id);
+    if (options.city) {
+      queryString += ' AND '
+    } else {
+      queryString += 'WHERE '
+    }
+    queryString += `owner_id = $${queryParams.length}`
+  }
+
+  if (options.minimum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night * 100);
+    if (options.city || options.owner_id) {
+      queryString += ' AND ';
+    } else {
+      queryString += 'WHERE '
+    }
+    queryString += `cost_per_night >= $${queryParams.length}`
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(options.maximum_price_per_night * 100);
+    if (options.city || options.owner_id || options.minimum_price_per_night) {
+      queryString += ' AND ';
+    } else {
+      queryString += 'WHERE '
+    }
+    queryString += `cost_per_night <= $${queryParams.length}`
+  }
+
+  queryString += `
+  GROUP BY properties.id`
+
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    queryString += ` HAVING avg(property_reviews.rating) >= $${queryParams.length}`
+  }
+
+  queryParams.push(limit);
+  queryString += 
+  ` ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  console.log(queryString, queryParams);
+
+  return pool.query(queryString, queryParams)
   .then(res => res.rows);
 }
 exports.getAllProperties = getAllProperties;
